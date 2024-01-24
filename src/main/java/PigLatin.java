9fa65a559aa4023b355c9353ba03b473597545b7
jachen28 <142927320+jachen28@users.noreply.github.com>
2024-01-24 08:52:49 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -7,10 +7,7 @@ public void setup()
     System.out.println(pigLatin(lines[i]));
   }
 }
-public void draw()
-{
-        //not used
-}
+
 public int findFirstVowel(String sWord)
 //precondition: sWord is a valid String of length greater than 0.
 //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
@@ -35,16 +32,21 @@ public String pigLatin(String sWord)
 //precondition: sWord is a valid String of length greater than 0
 //postcondition: returns the pig latin equivalent of sWord
 {
-  if(findFirstVowel(sWord) == 0) 
+  if(findFirstVowel(sWord) == 0) {
   return sWord + "way";
-  if(sWord.substring(0,2).equals("qu"))
+  }
+  if(sWord.substring(0,2).equals("qu")){
   return sWord.substring(2) + sWord.substring(0,2) + "ay";
-  if(findFirstVowel(sWord) == 1) 
+  }
+  if(findFirstVowel(sWord) == 1) {
   return sWord.substring(1) + sWord.substring(0,1) + "ay";
-  if(findFirstVowel(sWord) == 2) 
+  }
+  if(findFirstVowel(sWord) == 2) {
   return sWord.substring(2) + sWord.substring(0,2) + "ay";
-  if(findFirstVowel(sWord) == 3) 
+  }
+  if(findFirstVowel(sWord) == 3) {
   return sWord.substring(3) + sWord.substring(0,3) + "ay";
+  }
   if(findFirstVowel(sWord) == -1) {
   return sWord + "ay";
       }
